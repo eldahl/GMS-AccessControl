45ffdc710e7e5a315ef46a7c0c154ab1bb04e202
Pi <pi@greenmakerspace.dk>
2024-10-07 21:12:02 +0200

    Oh how you have tricked me, preventDefaults()

--- WebInterface/portal/views.py
+++ WebInterface/portal/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.template import loader
+from django.contrib.auth.models import User
 
 from .models import UserWithAccess
 from .models import LogEntry
